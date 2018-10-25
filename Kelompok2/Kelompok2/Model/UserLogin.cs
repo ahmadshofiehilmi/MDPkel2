b32dcdf19f7c353586b1b94e877edaaf66319e68
ahmadshofiehilmi <44147885+ahmadshofiehilmi@users.noreply.github.com>
2018-10-25 15:26:06 +0700

    Tugas MDP Modul 1 kelompok02

---
+++ Kelompok2/Kelompok2/Model/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kelompok2.Model
+{
+    public static class UserLogin
+    {
+        public static string Username = "Kelompok2";
+        public static string Password = "kel2";
+    }
+
+}
